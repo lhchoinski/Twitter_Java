ef88156e08f980e2977c71b3ed012cdbe4ebd192
aluno <Aluno@PR02-902-13.CSED.LAB>
2023-04-13 19:41:45 -0300

    teste

--- Pessoa.java
+++ Pessoa.java
@@ -1,4 +1,19 @@
 public class Pessoa{
 
-    
+    private String nome,login,email,senha;
+
+    public Pessoa(String nome, String login, String email, String senha) {
+        this.nome = nome;
+        this.login = login;
+        this.email = email;
+        this.senha = senha;
+    }
+
+
+    public 
+
+
+
+
+
 }
